2221e8f1d15dcf0cd7d5d27bc60c9095ce380774
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-04-16 19:00:27 +0900

    Feat: 파일 기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>안녕하세용</p>
+      <p>김준희입니당</p>
     </div>
   );
 }
